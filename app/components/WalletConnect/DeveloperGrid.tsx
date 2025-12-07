b398c567a31103f8976c3b8389e63231f3968c08
D3Portillo <d3portillo@gmail.com>
2025-12-08 00:32:18 +0100

    enh(DeveloperGrid): TOC over padding stuff

--- app/components/WalletConnect/DeveloperGrid.tsx
+++ app/components/WalletConnect/DeveloperGrid.tsx
@@ -2,8 +2,8 @@ import { initializeEruda } from "@/components/ErudaProdiver"
 
 export default function DeveloperGrid() {
   return (
-    <div className="border text-sm font-black flex gap-3.5 items-center -mt-2 border-white/10 rounded-lg p-2">
-      <div>DEV</div>
+    <div className="border text-sm font-black flex gap-3 items-center -mt-2 border-white/10 rounded-lg p-2">
+      <div className="pl-1">DEV</div>
       <button
         onClick={initializeEruda}
         className="bg-white text-black h-5 flex items-center px-2 rounded"
